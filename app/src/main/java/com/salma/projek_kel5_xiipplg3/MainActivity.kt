58516559a65096562734ca36c2892ad5cbc386f3
setyagita11 <umafagansa@gmail.com>
2023-10-11 09:57:28 +0700

    input admin

--- app/src/main/java/com/salma/projek_kel5_xiipplg3/MainActivity.kt
+++ app/src/main/java/com/salma/projek_kel5_xiipplg3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.salma.projek_kel5_xiipplg3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        android.os.Handler().postDelayed(
+            {
+                startActivity(
+                    Intent(this,login::class.java))
+                finish()
+            },3000
+        )
     }
 }
---
+++ app/src/main/java/com/salma/projek_kel5_xiipplg3/inputadmin.kt
@@ -0,0 +1,11 @@
+package com.salma.projek_kel5_xiipplg3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inputadmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inputadmin)
+    }
+}
