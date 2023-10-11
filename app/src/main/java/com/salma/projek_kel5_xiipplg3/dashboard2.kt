1fd0d17ed318761e535cf54cb80c9f1c66f25e5d
pysceseu <salma8232@smk.belajar.id>
2023-10-11 12:18:13 +0700

    act dashboard2 login

--- app/src/main/java/com/salma/projek_kel5_xiipplg3/dashboard2.kt
+++ app/src/main/java/com/salma/projek_kel5_xiipplg3/dashboard2.kt
@@ -3,9 +3,9 @@ package com.salma.projek_kel5_xiipplg3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class login : AppCompatActivity() {
+class dashboard2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_dashboard2)
     }
 }
