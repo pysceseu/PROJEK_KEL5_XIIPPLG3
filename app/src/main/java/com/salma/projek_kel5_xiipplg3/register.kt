eb9bef24e1a8668fcab5e2712e1d778923a53fcd
pysceseu <salma8232@smk.belajar.id>
2023-10-11 12:25:02 +0700

    act register

--- app/src/main/java/com/salma/projek_kel5_xiipplg3/register.kt
+++ app/src/main/java/com/salma/projek_kel5_xiipplg3/register.kt
@@ -3,9 +3,9 @@ package com.salma.projek_kel5_xiipplg3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class registrasi : AppCompatActivity() {
+class register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registrasi)
+        setContentView(R.layout.activity_register)
     }
 }
